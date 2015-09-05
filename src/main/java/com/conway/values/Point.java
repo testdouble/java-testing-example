2388cec0867a5a77c51a1f86168449ff1d789c7a
Justin Searls <searls@gmail.com>
2015-09-04 19:25:18 -0500

    Failing test verified Point#equals needed
    GathersNeighbors fails without point equality.

--- src/main/java/com/conway/values/Point.java
+++ src/main/java/com/conway/values/Point.java
@@ -14,35 +14,35 @@ public class Point {
     return contents.getClass().getName() + " at ( "+coordinates.x+", "+coordinates.y+")";
   }
 
-//  @Override
-//  public int hashCode() {
-//    final int prime = 31;
-//    int result = 1;
-//    result = prime * result + ((contents == null) ? 0 : contents.hashCode());
-//    result = prime * result + ((coordinates == null) ? 0 : coordinates.hashCode());
-//    return result;
-//  }
-//
-//  @Override
-//  public boolean equals(Object obj) {
-//    if (this == obj)
-//      return true;
-//    if (obj == null)
-//      return false;
-//    if (!(obj instanceof Point))
-//      return false;
-//    Point other = (Point) obj;
-//    if (contents == null) {
-//      if (other.contents != null)
-//        return false;
-//    } else if (!contents.equals(other.contents))
-//      return false;
-//    if (coordinates == null) {
-//      if (other.coordinates != null)
-//        return false;
-//    } else if (!coordinates.equals(other.coordinates))
-//      return false;
-//    return true;
-//  }
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((contents == null) ? 0 : contents.hashCode());
+    result = prime * result + ((coordinates == null) ? 0 : coordinates.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof Point))
+      return false;
+    Point other = (Point) obj;
+    if (contents == null) {
+      if (other.contents != null)
+        return false;
+    } else if (!contents.equals(other.contents))
+      return false;
+    if (coordinates == null) {
+      if (other.coordinates != null)
+        return false;
+    } else if (!coordinates.equals(other.coordinates))
+      return false;
+    return true;
+  }
 
 }
