dd2408e78779087b9b0d0eb12b6d6ca911ec7e51
Justin Searls <searls@gmail.com>
2016-07-26 10:12:41 -0400

    Remove JavaScript configuration

--- src/main/js/example/hello-world-proxy.js
+++
@@ -1,9 +0,0 @@
-window.App = window.App || {};
-
-App.HelloWorldProxy = function(helloWorld){
-  this.helloWorld = helloWorld;
-};
-
-App.HelloWorldProxy.prototype.say = function() {
-  return this.helloWorld.say();
-};
--- src/main/js/example/hello-world.js
+++
@@ -1,5 +0,0 @@
-window.App = window.App || {};
-
-App.HelloWorld = function(){};
-
-App.HelloWorld.prototype.say = function() { return "Hello, World"; };
